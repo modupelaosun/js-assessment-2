2585e93110c76cf9568b282e0fd1a6b667162786
Modupe <laosunmodupe@gmail.com>
2024-02-11 21:23:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//  question 1. How can you select an element by its ID using JavaScript?
+let myname = document.getElementById(myname)
+
+
+// question 2. How can you select all elements of a specific class name?
+
+const elements = document.querySelectorAll('.example')
+
+
+// question 3. How can you select the first child element of a given element?
+
+let myContainer = document.getElementById("Container")
+let allChildren = myContainer.children;
+
+console.log(allChildren[0] )
+
+
+// question 4. ● How can you select all elements that match a specific CSS selector?
